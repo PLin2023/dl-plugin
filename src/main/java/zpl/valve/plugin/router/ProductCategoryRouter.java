87124339c15b989eb3773a13585c8494c72440ac
zhangpeilin <zhangpeilin@adswave.com>
2024-08-22 18:48:59 +0800

    媳妇

--- src/main/java/zpl/valve/plugin/router/ProductCategoryRouter.java
+++ src/main/java/zpl/valve/plugin/router/ProductCategoryRouter.java
@@ -20,16 +20,16 @@ public class ProductCategoryRouter {
     private final TemplateNameResolver templateNameResolver;
     @Bean
     RouterFunction<ServerResponse> productListRouterFunction() {
-        return route(GET("/productlist/{id}"), this::renderProductListPage);
+        return route(GET("/productlist/{id}/{page}"), this::renderProductListPage);
     }
 
     Mono<ServerResponse> renderProductListPage(ServerRequest request) {
         String id = request.pathVariable("id");
+        String page = request.pathVariable("page");
         var model = new HashMap<String, Object>();
         model.put("id", id);
+        model.put("page", page);
         return templateNameResolver.resolveTemplateNameOrDefault(request.exchange(), "productlist")
             .flatMap(templateName -> ServerResponse.ok().render(templateName, model));
     }
-
-
 }
